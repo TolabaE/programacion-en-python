db5f471842dfc27d65574d92d9599c679ef6d9c2
Eduardo Tolaba <eduardoetolaba@gmail.com>
2024-05-03 00:26:37 -0300

    agragdo de teoria,metodos de manipulacion de cadenas de texto

--- src/utils/string.py
+++ src/utils/string.py
@@ -11,4 +11,28 @@ numeroString = "2024";
 
 #para recortar palabras de una cadena de texto
 primera_palabra = saludo[0:4]
-print(primera_palabra)
+
+
+print(len(saludo))#calcula la longuitud de caracter.
+
+#OPERADOR DE CONCATENACION
+nombre = "camila"
+apellido ="cardozo"
+
+nombre_completo = nombre + apellido
+
+#SEGUNDA FORMA DE CONCATENAR
+
+#la f es un operador de formateo.
+nombreCompleto = f"{nombre} {apellido}"
+
+
+#METODOS CON STRINGS
+
+texto = "asi es la vida al final que nos queda, tu esocojiste el camino y yo la vereda"
+
+print(texto.upper())#pasa todo a mayuscula
+print(texto.lower())#pasa todo a minuscula
+print(texto.capitalize())#convierte la primera letra en mayuscula
+print(texto.title())#convierte todas las primeras palabras en mayuscula
+print(texto.strip())#elimina todos los espacios de la izquierda a la derecha
