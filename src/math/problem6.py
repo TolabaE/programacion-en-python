1bd56ac211b6f3e7873d8471e85c7f529c0272e5
Eduardo Tolaba <eduardoetolaba@gmail.com>
2024-05-03 20:09:29 -0300

    resolucion del ejercicio 6, conversion de moneda}

---
+++ src/math/problem6.py
@@ -0,0 +1,7 @@
+# CONVERSION DE MONEDA
+# Escribir un programa que convierta una cantidad de pesos argentinos a dólares estadounidenses, utilizando un tipo de cambio fijo. El usuario debe ingresar la cantidad de pesos.
+
+def covertirMoneda():
+    valor = input("ingrese el valor de la moneda en pesos ARG que quiere pasar a dolar: ")
+    dolarUSD = int(valor) / 877
+    print("su cambio de ",valor, "pasado  a dolar le da como resultado: usd ",dolarUSD)
