004e3bbfcd057260771bf7768a952c9b00b1d6ab
Eduardo Tolaba <eduardoetolaba@gmail.com>
2024-05-10 21:07:10 -0300

    resolucion del ejercicio 8, tecnicas de programacion

---
+++ src/math/problem8.py
@@ -0,0 +1,8 @@
+# ESCRIBIR UN ALGORITMO QUE CALCULE LA RAÍZ CUADRADA DE UN NÚMERO INGRESADO POR EL USUARIO.
+
+def calculoRaizCuadrada():
+    numero = int(input("ingrese un numero que desea calcular su raiz cuadrada: "))
+    # print(type(numero))
+    raiz = int((numero)**0.5)
+    print("el resultado de la raiz cuadrada de",numero, "es igual a:",raiz)
+    
