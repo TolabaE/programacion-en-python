462b2ad124177a21b05cd5db564a4fd79f7ae388
Eduardo Tolaba <eduardoetolaba@gmail.com>
2024-05-15 11:02:11 -0300

    agragado de algunos operaciones con numeros

--- src/math/problem9.py
+++ src/math/problem9.py
@@ -6,5 +6,5 @@ import math
 def calcularRadio():
     radio = input("ingrese el radio(en cm) de un circulo para calcular su area: ")
     pi= 3.14
-    areaCirculo = pi * (int(radio,base=10)**2)
+    areaCirculo = pi * (int(radio,base=0)**2)
     print("el area del circulo es de: ",areaCirculo,"cm")
---
+++ src/utils/number.py
@@ -0,0 +1,31 @@
+# En este archivo, tenemos algunos ejemplo de manipulacion de numeros, ya se los tipos de numero que podemos tener y algunos metodo que nos ofrece
+# python para la manipulacion de algunos problemas, usando una de las librerias integradas llamada "math".
+
+
+# TIPOS DE NUMEROS
+entero = 10 
+decimal = 4.2
+imaginarios = 2 + 2j # 2i es un numero imaginario
+
+suma = 1+14
+resta = 20-10
+multiplicacion = 6*8
+division = 21/3
+division2 = 1//3 # nos retorna la division pero sin los decimales, unicamnete numero entero.
+divModulo = 20 % 3 # nos retorna el resto de una division.
+potencia = 4**3 #calcular su potencia.
+
+# formas cortas de escribir: numero = numero +8
+number = 4
+number +=4
+number -=4
+number /=4
+number *=4
+
+# FUNCIONES NATIVAS DE PYTHON PARA OPERACIONES.
+print(round(1.2))# Me retorna si el valor esta mas cerca del 1 o del 2 en teste caso esta mas cerca del 1
+print(round(1.7)) # En este caso esta mas cerca del 2
+print(abs(-120)) # nos retorna siempre un valor positivo.
+
+# HACIENDO USO DEL MODULO NATIVO DE PYTHON LLAMADO "math".
+
