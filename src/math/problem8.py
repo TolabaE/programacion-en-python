120a0d17a38dfaad4df8e36863142c9c446f67e7
Eduardo Tolaba <eduardoetolaba@gmail.com>
2024-05-15 16:36:52 -0300

    arreglos en el codigo del ejercicio 8 y 9, obtimizacion en el algoritmo

--- src/math/problem8.py
+++ src/math/problem8.py
@@ -1,9 +1,8 @@
 # ESCRIBIR UN ALGORITMO QUE CALCULE LA RAÍZ CUADRADA DE UN NÚMERO INGRESADO POR EL USUARIO.
 # ES UNA ESTRUCTURA DE CONTROL SECUENCIAL DE TIPO FUNCIONAL
+import math
 
 def calculoRaizCuadrada():
     numero = int(input("ingrese un numero que desea calcular su raiz cuadrada: "))
-    # print(type(numero))
-    raiz = int((numero)**0.5)
+    raiz = math.sqrt(numero) #metodo que obtiene la raiz cuadrada.
     print("el resultado de la raiz cuadrada de",numero, "es igual a:",raiz)
-    
--- src/math/problem9.py
+++ src/math/problem9.py
@@ -1,10 +1,8 @@
 # ESCRIBIR UN PROGRAMA QUE CALCULE EL AREA DE UN CIRCULO, DADO SU RADIO. EL USUARIO DEBE INGRESAR EL VALOR DEL RADIO.
 # ES UNA ESTRUCTURA DE CONTROL SECUENCIAL DE TIPO FUNCIONAL
-import math
-
 
 def calcularRadio():
-    radio = input("ingrese el radio(en cm) de un circulo para calcular su area: ")
-    pi= 3.14
-    areaCirculo = pi * (int(radio,base=0)**2)
-    print("el area del circulo es de: ",areaCirculo,"cm")
+    radio = float(input("ingrese el radio(en cm) de un circulo para calcular su area: "))
+    pi = 3.12
+    areaCirculo = pi * (radio**2)
+    print("el area del circulo es de: ",areaCirculo,"cm2")
