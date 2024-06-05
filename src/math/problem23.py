e0444148748aaecf50a9b920b35d34f1307c52fb
Eduardo Tolaba <eduardoetolaba@gmail.com>
2024-06-04 22:13:29 -0300

    resolucion del ejercicio 23, tecnicas de programacion

---
+++ src/math/problem23.py
@@ -0,0 +1,6 @@
+# Se requiere un algoritmo que calcule el sueldo neto de un trabajador. Para ello, el algoritmo debe admitir el ingreso del monto a cobrar por horas y el total de horas trabajadas. Si el empleado trabajo más de 160 horas
+# mensuales se deben considerar la diferencia como horas extras y el monto por hora deberá ser el doble del valor ingresado en un inicio.
+
+sueldo = int(input("Ingrese el sueldo del trabajador"))
+horas = int(input("Ingrese en minutos la cantidad de horas trabajadas del empleado"))
+
