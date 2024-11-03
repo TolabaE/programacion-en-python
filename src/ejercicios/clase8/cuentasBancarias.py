bca06578d771bf11e0bc26b69ada1b6997871d9b
Eduardo Tolaba <eduardoetolaba@gmail.com>
2024-11-02 23:11:55 -0300

    guardado de cambios, trabajo practico clase 8, no terminado

--- src/ejercicios/clase8/cuentasBancarias.py
+++ src/ejercicios/clase8/cuentasBancarias.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from abc import abstractmethod,ABC
 
 
-
-class CuentaBancaria:
+# clase abstracta
+class CuentaBancaria(ABC):
     
     def __init__(self,nro_cuentas:str,cbu:str,alias:str,saldo:float,movimientos:list):
         self.nro_cuenta = nro_cuentas
@@ -36,9 +36,34 @@ class CuentaBancaria:
         pass
 
 
+class CajaDeAhorro(CuentaBancaria):
+
+    def __init__(self, nro_cuentas, cbu, alias, saldo, movimientos, monto_limite_extracciones:float,monto_limite_transferencia:float,cant_extracciones_disponible:int,cant_transferencia_disponible:int):
+        super().__init__(nro_cuentas, cbu, alias, saldo, movimientos)
+        self.limite_extraccion = monto_limite_extracciones
+        self.limite_transferencia = monto_limite_transferencia
+        self.cant_extracciones_disponible = cant_extracciones_disponible
+        self.cant_transferencia_disponible = cant_transferencia_disponible
+
+    def extraer(self,monto_a_extraer:float):
+        saldo = self.consultar_saldo() # invoco al metodo para verificar su saldo en cuenta
+        if(monto_a_extraer == 0): return f"no se puede realizar una trasferencia de 0 , intente otro monto"
+        if(monto_a_extraer > saldo): return f"el limite de dinero a extraer  es superior a la cantidad disponible"
+
+        if(monto_a_extraer <= self.limite_extraccion and self.cant_extracciones_disponible > 0):
+            self.saldo = self.saldo - monto_a_extraer
+            self.cant_extracciones_disponible -= 1 
+            print("su extracion a sido realizo correctamente")
+        else:
+            print("el limite de dinero a extraer superior a la cantidad disponible")
 
 
-cliente1 = CuentaBancaria(12345,182983,"edu.mp",15000,[])
-cliente1.depositar(1200)
 
-print(cliente1.movimientos)
+usuario1 = CajaDeAhorro("12345", "11030", "eduardo-cbu19", 112000, [], 50000, 100000, 5, 10)
+
+# cliente1 = CuentaBancaria(12345,182983,"edu.mp",15000,[])
+# cliente1.depositar(1200)
+
+response = usuario1.extraer(0)
+print(response)
+# print(cliente1.movimientos)
